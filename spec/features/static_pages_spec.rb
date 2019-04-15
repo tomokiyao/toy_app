10f287bca0123952423ee92faaf525afbda88300
tomokiyao <tomoki9226@gmail.com>
2019-04-15 11:58:43 +0900

    変更途中

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 feature 'static_pages', type: :feature do
 
   it 'home title' do
-    visit static_pages_home_path
+    visit home_path
     expect(page).to have_title('HOME|Ruby on Rails Tutorial')
   end
 
   it 'help title' do
-    visit static_pages_help_path
+    visit help_path
     expect(page).to have_title('HELP|Ruby on Rails Tutorial')
   end
 
   it 'about title' do
-    visit static_pages_about_path
+    visit about_path
     expect(page).to have_title('ABOUT|Ruby on Rails Tutorial')
   end
 
   it 'contact title' do
-    visit static_pages_contact_path
+    visit contact_path
     expect(page).to have_title('CONTACT|Ruby on Rails Tutorial')
   end
 
